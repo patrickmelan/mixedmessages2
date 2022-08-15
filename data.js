51e73d1e4b88cd800438737e271e5ea93fc8ec1e
Emily <heyemmers@gmail.com>
2022-08-15 13:36:14 -0400

    makes another data set that separates the quote and author

--- data.js
+++ data.js
@@ -4,11 +4,22 @@ function syncReadFile(filename) {
     const contents = readFileSync(filename, 'utf-8');
 
     const data = contents.split(/\r?\n/);
-    console.log(data);
 
     return data;
 }
-//data is in format data = ['quote. ~Author Name', 'quote. ~ Author Name', ....]
+//data is in format data = ['You may have to fight a battle more than once to win it. ~ Margaret Thatcher', ]
 const data = syncReadFile('./doc.txt')
 
-module.exports = {data}
+//sorts data in fromat dataSortedByQuoteAndAuthor = [ {quote: 'You may have to fight a battle...', author: 'Margaret Thatcher'}]
+const dataSortedByQuoteAndAuthor = []
+for (let i=0; i<data.length; i++){
+    console.log
+    const quote_author = data[i].split('~');
+    const quote = quote_author[0];
+    const author = quote_author[1];
+    dataSortedByQuoteAndAuthor.push({"quote":quote, "author":author})
+}
+console.log(dataSortedByQuoteAndAuthor);
+
+//export format to use data, dataSortedByQuoteAndAuthor to all files but only button.js will require/use it
+module.exports = {data, dataSortedByQuoteAndAuthor}
