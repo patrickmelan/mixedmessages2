2b850a0fbf8635503f81da663b40f5ddc87d2679
Emily <heyemmers@gmail.com>
2022-08-15 11:54:12 -0400

    populates doc text with quotes, extracts doc text into data array and sends it to button.js

--- data.js
+++ data.js
@@ -1,1 +1,14 @@
+const {readFileSync, promises: fsPromises} = require('fs');
 
+function syncReadFile(filename) {
+    const contents = readFileSync(filename, 'utf-8');
+
+    const data = contents.split(/\r?\n/);
+    console.log(data);
+
+    return data;
+}
+//data is in format data = ['quote. ~Author Name', 'quote. ~ Author Name', ....]
+const data = syncReadFile('./doc.txt')
+
+module.exports = {data}
